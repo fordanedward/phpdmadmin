aef262b5dea4b3cddbf356b0faf78ef1b564055a
fordanedward <202210106@gordoncollege.edu.ph>
2025-08-30 13:38:30 +0800

    Update +page.svelte

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -422,7 +422,7 @@
                 </div>
                 <div class="register-button {isPageLoaded ? 'loaded' : ''} mb-4">
                     <Button type="submit" class="w-full p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-                        Register as {selectedRole === 'userDentist' ? 'Dentist' : 'Secretary'}
+                        Register as {selectedRole === 'userDentist' ? 'Doctor' : 'Secretary'}
                     </Button>
                 </div>
             </form>
@@ -440,7 +440,7 @@
                     class="w-full p-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 flex items-center justify-center"
                 >
                     <GoogleSolid class="w-5 h-5 mr-2" />
-                    {isGoogleSigningIn ? 'Connecting...' : `Sign up with Google as ${selectedRole === 'userDentist' ? 'Dentist' : 'Secretary'}`}
+                    {isGoogleSigningIn ? 'Connecting...' : `Sign up with Google as ${selectedRole === 'userDentist' ? 'Doctor' : 'Secretary'}`}
                 </Button>
             </div>
 
