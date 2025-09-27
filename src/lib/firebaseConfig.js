481dfac39647c8eed9a904ac4f947e4e97a733a5
EdwardFordan <132804180+fordanedward@users.noreply.github.com>
2025-09-27 19:30:17 +0800

    google sign in

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNGNakXXmB89nR5-JOYcMOMAEDCTS9WjE",
-  authDomain: "integratedsystem-4040b.firebaseapp.com",
-  projectId: "integratedsystem-4040b",
-  storageBucket: "integratedsystem-4040b.firebasestorage.app",
-  messagingSenderId: "529987505201",
-  appId: "1:529987505201:web:e36fd3e66c584da48f1910",
-  measurementId: "G-4QT0RK92C0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDNGNakXXmB89nR5-JOYcMOMAEDCTS9WjE",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "integratedsystem-4040b.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "integratedsystem-4040b",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "integratedsystem-4040b.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "529987505201",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:529987505201:web:e36fd3e66c584da48f1910",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-4QT0RK92C0"
 };
 
 
