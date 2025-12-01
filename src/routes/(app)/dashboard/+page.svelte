3de74bdfd5790e8b72cb99d01ed1e84aff0a7b32
fordanedward <202210106@gordoncollege.edu.ph>
2025-12-01 14:41:18 +0800

    Update +page.svelte

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1813,7 +1813,7 @@ function downloadExcelReportFromReport(
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 sm:h-5 sm:w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
 							</svg>
-							<span class="hidden sm:inline">Reports</span>
+							<span>Reports</span>
 						</button>
 					</div>
 				</div>
